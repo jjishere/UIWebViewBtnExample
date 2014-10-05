00425e8036c0deb9ecf118c9f46302c5e66c35a8
JJ <Admin@Admins-MBP-3.home>
2014-10-05 16:35:53 -0700

    Initial Commit

---
+++ UIWebViewBtnExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIWebViewBtnExample
+//
+//  Created by JJ on 10/5/14.
+//  Copyright (c) 2014 appsharenow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIWebViewBtnExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UIWebViewBtnExample
+//
+//  Created by JJ on 10/5/14.
+//  Copyright (c) 2014 appsharenow. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
